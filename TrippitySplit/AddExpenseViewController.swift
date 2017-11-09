3898b97d522bcddcd454ea3c196ace572688e496
graciefulgirl <graciefulgirl@gmail.com>
2017-11-08 16:10:20 -0800

    Added Core Data to sore expense name

---
+++ TrippitySplit/AddExpenseViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AddExpenseViewController.swift
+//  TrippitySplit
+//
+//  Created by admin on 11/8/17.
+//  Copyright © 2017 graciefulgirl. All rights reserved.
+//
+
+import UIKit
+
+class AddExpenseViewController: UIViewController {
+    @IBOutlet weak var expenseNameTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addExpenseButtonTapped(_ sender: UIButton) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let expense = Expense(context: context) // Link Expense & Context
+        expense.name = expenseNameTextField.text!
+        
+        // Save the data to coredata
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        let _ = navigationController?.popViewController(animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TrippitySplit/TrippitySplit/ExpensesTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ExpensesTableViewController.swift
+//  TrippitySplit
+//
+//  Created by admin on 11/8/17.
+//  Copyright © 2017 graciefulgirl. All rights reserved.
+//
+
+import UIKit
+
+class ExpensesTableViewController: UITableViewController {
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var expenses: [Expense] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        getData()
+        tableView.reloadData()
+    }
+    
+    func getData() {
+        do {
+            expenses = try context.fetch(Expense.fetchRequest())
+        } catch {
+            print("Fetching Failed")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return expenses.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let expense = expenses[indexPath.row]
+        
+        if let name = expense.name {
+            cell.textLabel?.text = name
+        }
+        return cell
+    }
+    
+
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let expense = expenses[indexPath.row]
+            context.delete(expense)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            do {
+                expenses = try context.fetch(Expense.fetchRequest())
+            } catch {
+                print("Fetch Failed")
+            }
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+}
